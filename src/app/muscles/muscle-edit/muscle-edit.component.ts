72b2806558b8b5992b85b272e197ca47fa600a74
pec05 <leandro-peccio@hotmail.com>
2022-09-06 15:18:48 +0200

    muscle edit component

--- src/app/muscles/muscle-edit/muscle-edit.component.ts
+++ src/app/muscles/muscle-edit/muscle-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Muscle } from '../muscle';
+import { MuscleService } from '../muscle.service';
 
 @Component({
   selector: 'app-muscle-edit',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MuscleEditComponent implements OnInit {
 
-  constructor() { }
+  muscle: Muscle;
+  errorMessage: string;
 
-  ngOnInit(): void {
+  constructor(private muscleService: MuscleService, private route: ActivatedRoute, private router: Router) {
+    this.muscle = {} as Muscle;
+   }
+
+  ngOnInit() {
+    const muscleID = this.route.snapshot.params['id'];
+    this.muscleService.getMuscleByID(muscleID).subscribe(
+      muscle => this.muscle = muscle,
+      error => this.errorMessage = error as any
+    );
   }
 
+  onSubmit(muscle: Muscle) {
+    this.muscleService.addAndUpdateMuscle(muscle).subscribe(
+      response => {
+        console.log('updated success');
+        this.onBack();
+      },
+      error => this.errorMessage = error as any
+    );
+  }
+
+  onBack() {
+    this.router.navigate(['/muscles']);
+  }
 }
